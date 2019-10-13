ed8871291ff8a53d026ab1ba41705c7e9fc98175
Bill Tanthowi Jauhari <bill.tanthowi.j@gmail.com>
2019-10-13 18:55:46 +0700

    Initial Commit

---
+++ CalcLibrary/CalcLibrary.h
@@ -0,0 +1,19 @@
+//
+//  CalcLibrary.h
+//  CalcLibrary
+//
+//  Created by Bill Tanthowi Jauhari on 13/10/19.
+//  Copyright © 2019 TechnologueID. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CalcLibrary.
+FOUNDATION_EXPORT double CalcLibraryVersionNumber;
+
+//! Project version string for CalcLibrary.
+FOUNDATION_EXPORT const unsigned char CalcLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CalcLibrary/PublicHeader.h>
+
+
